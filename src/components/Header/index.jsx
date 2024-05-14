8027fae308b651c75aab47b9e00488e68a32c5fa
Leonardo <leo.santosp@outlook.com>
2024-05-14 13:01:31 -0300

    Upload new classes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
-import { Container, Profile } from './styles';
+import { RiShutDownLine } from 'react-icons/ri';
+import { Container, Profile, Logout } from './styles';
 
 export function Header(){
     return(
@@ -14,6 +15,9 @@ export function Header(){
                     <strong>Leonardo Santos</strong>
                 </div>
             </Profile>
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
         </Container>
     )
 }
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,56 @@
-import { Container } from './styles';
+import { Container, Links, Content } from './styles';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
+import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
+import { ButtonText } from '../../components/ButtonText';
+
+
+
 
 export function Details(){
 
     return(
         <Container>
-            <Header></Header>
-            <Button title="Voltar"></Button>
+            <Header/>
+            <main>
+                <Content>
+
+                    <ButtonText title="Excluir nota"></ButtonText>
+
+                    <h1>Introdução ao React</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                       A corrupti alias adipisci soluta odit. Iusto ut rem nostrum magnam, 
+                       unde consequuntur accusamus minima incidunt? Voluptate dolor optio repudiandae omnis quos!
+                       Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                       A corrupti alias adipisci soluta odit. Iusto ut rem nostrum magnam, 
+                       unde consequuntur accusamus minima incidunt? Voluptate dolor optio repudiandae omnis quos!</p>
+                    
+                    <Section title="Links úteis">
+                        <Links>
+                            <li>
+                                <a href="#">https://www.rocketseat.com.br/</a>
+                            </li>
+                            <li>
+                                <a href="#">https://www.rocketseat.com.br/</a>
+                            </li>
+                            <li>
+                                <a href="#">https://www.rocketseat.com.br/</a>
+                            </li>
+                        </Links>
+                    </Section>
+
+                    <Section title="Marcadores">
+                        <Tag title="express"/>
+                        <Tag title="nodejs"/>
+                    </Section>
+
+                    <Button title="Voltar"></Button>
+                </Content>
+            </main>
         </Container>
     )
 }
+
+/* Dentro do 'Section' que nós passamos o children */
